a082a8e54cb632ab80ac2ba2eda32061eb3934eb
Jack Lewis <jack@jacklew.is>
2022-01-14 15:59:43 -0500

    Improved fetch stock encore script

--- scripts/fetch_stock_encore.sh
+++ scripts/fetch_stock_encore.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 while true; do
-  read -p "This will delete your existing enCore database, are you sure?" yn
+  read -rep $'This will delete your existing enCore database, are you sure?\n> ' yn
   case $yn in
     [Yy]* ) rm -rf encore/* db/*; break;;
-    [Nn]* ) exit;
+    [Nn]* ) exit;;
     * ) echo "Please answer yes or no.";;
   esac
 done
 
-git clone --depth=1 https://github.com/rhinoceraptor/enCoreV4.git encore_temp
+echo "Cloning stock enCore database..."
+git clone --quiet --depth=1 https://github.com/rhinoceraptor/enCoreV4.git encore_temp
+echo "Placing files..."
 mv encore_temp/enCore.db* db/
 mv encore_temp/* encore/
+echo "Cleaning up temp files..."
 rm -rf encore_temp
+echo "Done!"
