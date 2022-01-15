8af28f3b7e931d2644feaa858ad1b5f40ce5ae39
Jack Lewis <jack@jacklew.is>
2022-01-15 18:55:18 -0500

    Added nginx HTTP mod to rewrite links in HTML

--- TODO.md
+++ TODO.md
@@ -2,6 +2,9 @@
 
 - [ ] Implement `configure_encore.sh` script
   - `./moo -e` is emergency mode for editing encore DBs
+  - [ ] `@configure`, various other verbs/properties
+- [x] Nginx content rewrite HTTPS
+- [ ] Reverse proxy upstream server
 - [x] Implement `fetch_stock_encore.sh` script
 - [x] Don't expose enCore db for stock DB usage
 - [ ] Let's Encrypt
--- nginx/conf.d/rewrite.js
+++ nginx/conf.d/rewrite.js
@@ -1,6 +1,22 @@
+var useHTTPS = process.env.USE_HTTPS === 'true';
+var ourDomain = process.env.DOMAIN_NAME;
+var externalProxyPath = process.env.EXTERNAL_PROXY_PATH;
 
 function rewriteBodyLinks(r, data, flags) {
-  return r.sendBuffer(data.replace('http://', 'https://'));
+  if (!useHTTPS) {
+    return r.sendBuffer(data);
+  }
+
+  var ourDomainRegex = new RegExp('"http://(.*?)"', 'gi')
+  var newData = data.replace(ourDomainRegex, function (URL, schemalessURL) {
+    var prefix = URL.startsWith(`http://${ourDomain}`)
+      ? ''
+      : `${ourDomain}/${externalProxyPath}/`;
+
+    return `"https://${prefix}${schemalessURL}"`;
+  });
+
+  return r.sendBuffer(newData);
 }
 
-export default rewriteBodyLinks;
+export default { rewriteBodyLinks };
