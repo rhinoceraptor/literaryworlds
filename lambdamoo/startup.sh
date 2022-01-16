337621c1ef4a86c7c65e2038232349b2b41f4be3
Jack Lewis <jack@jacklew.is>
2022-01-16 02:42:33 -0500

    Weird bug with backing up DBs

--- lambdamoo/startup.sh
+++ lambdamoo/startup.sh
@@ -5,25 +5,34 @@ set -e
 export DATABASE=$1
 export NEWDATABASE=$2
 
+ls -lA /db/
+
 # Configure the database
-echo -e "\n=============================================================="
+echo -e "\n==============================================================="
 echo -e "               Configuring LambdaMOO Database"
-echo -e "==============================================================\n"
+echo -e "===============================================================\n"
 ./configure_db
 
-DB_BACKUP="$DATABASE.$(date +%F-%X)"
-echo -e "\n=============================================================="
-echo -e " Backing up $DATABASE to $DB_BACKUP"
-echo -e "==============================================================\n"
-# Create a backup of the old database
-mv "$DATABASE" "$DATABASE.$DB_BACKUP"
+ls -lA /db/
+ls -lA "$DATABASE"
 
+DB_BACKUP="$DATABASE.$(date +%F-%X)"
+ls -lA "$DATABASE"
+echo -e "\n==============================================================="
+echo -e " Backing up $DATABASE to $DB_BACKUP"
+echo -e "===============================================================\n"
+# Create a backup of the old database
+mv -i "$DATABASE" "$DATABASE.$DB_BACKUP"
+
+echo -e "\n==============================================================="
+echo -e " Backing up $NEWDATABASE to $DATABASE"
+echo -e "===============================================================\n"
 # Move the edited database
-mv "$NEWDATABASE" "$DATABASE"
+mv -i "$NEWDATABASE" "$DATABASE"
 
 # Start up LambdaMOO!
-echo -e "\n=============================================================="
+echo -e "\n==============================================================="
 echo -e "                    Starting LambdaMOO"
-echo -e "==============================================================\n"
+echo -e "===============================================================\n"
 exec ./moo "$DATABASE" "$NEWDATABASE"
 
